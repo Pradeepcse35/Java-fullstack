d95f483f67678beba064fc0bf91a82215ba753cb
PRADEEP R <132440472+Pradeepcse35@users.noreply.github.com>
2024-09-05 13:40:38 +0530

    Add files via upload

---
+++ JS tasks/switch.js
@@ -0,0 +1,27 @@
+for (let i = 1; i <= 7; i++) {
+    switch (i) {
+      case 1:
+        console.log("Monday - 1");
+        break;
+      case 2:
+        console.log("Tuesday - 2");
+        break;
+      case 3:
+        console.log("Wednesday - 3");
+        break;
+      case 4:
+        console.log("Thursday - 4");
+        break;
+      case 5:
+        console.log("Friday - 5");
+        break;
+      case 6:
+        console.log("Saturday - 6");
+        break;
+      case 7:
+        console.log("Sunday -7");
+        break;
+    }
+  }
+  
+  
